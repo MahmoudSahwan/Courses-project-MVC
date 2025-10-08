c76892762a59de6d1fbd3a6a16b434697f9ae5e6
MahmoudSahwan <m.sahwan1995@gmail.com>
2025-10-08 05:14:43 +0200

    init Models

--- Models/Course.cs
+++ Models/Course.cs
@@ -1,6 +1,18 @@
-﻿namespace CoursesMVC.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CoursesMVC.Models
 {
     public class Course
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int Degree { get; set; }
+        public int MinDegree { get; set; }
+
+        [ForeignKey("Department")]
+        public int DepartmentId { get; set; }
+
+        public Department Department { get; set; }
     }
 }
--- Models/CrsResult.cs
+++ Models/CrsResult.cs
@@ -1,7 +1,14 @@
-﻿namespace CoursesMVC.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CoursesMVC.Models
 {
     public class CrsResult
     {
+        public int Id { get;set; }
+        public string Degree { get; set; }
 
+        [ForeignKey("Course")]
+        public int CourseId { get; set; }
+        public Course Course { get; set; }
     }
 }
